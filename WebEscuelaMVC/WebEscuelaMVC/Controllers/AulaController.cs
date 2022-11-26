ae96cea056e97c4edc2ffafbcb97e6c37b0bdb83
FlorenciaPalazzo <florencia.e.palazzo@gmail.com>
2022-11-26 13:59:08 -0300

    “EscuelaDBMVC”-Tarea 6-Finalizada

---
+++ WebEscuelaMVC/WebEscuelaMVC/Controllers/AulaController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebEscuelaMVC.Data;
+using WebEscuelaMVC.Models;
+
+namespace WebEscuelaMVC.Controllers
+{
+    public class AulaController : Controller
+    {
+        //Instancia
+        private AulaDBContext context = new AulaDBContext();
+
+        // GET: Aula
+        public ActionResult Index()
+        {
+            List<Aula> lista = context.Aulas.ToList();
+            return View("Index",lista);
+        }
+
+        //GET: Aula/Create
+        public ActionResult Create() 
+        {
+            Aula aula = new Aula();
+            return View("Create", aula);
+         }
+
+        //POST: Aula/Create
+        public ActionResult Create(Aula aula)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Aulas.Add(aula);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View("Create");
+
+        }
+        //GET: Aula/id
+        public ActionResult Detail(int id)
+        {
+            Aula aula = context.Aulas.Find(id);
+            if (aula == null)
+            {
+                HttpNotFound();
+            }
+            return View("Detail", aula);
+
+        }
+
+
+    }
+}
